3c3f232a81e4e33f756ed07123df2376ee4ef0d1
shashankmn <shashank.1si12cs111@gmail.com>
2017-05-16 00:14:08 +0530

    added middlewares and a couple basic express methods

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("hello shashank");
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+var app = express();
+
+
+// var logger = function(req,res,next) {
+// console.log("logging...");
+// next();
+// }
+//
+// app.use(logger);
+
+// body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+// set static path
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/',function(req,res){
+  res.send("hello people!!");
+});
+
+
+app.listen(3000, function() {
+console.log("App started on port 3000!! :)");
+});
