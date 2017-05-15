e5adf4452d7263202e0b853c3587a2207c83db77
shashankmn <shashank.1si12cs111@gmail.com>
2017-05-16 01:43:59 +0530

    adding forms to the application along with the express- validators validation for form inputs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var expressValidator = require("express-validator");
 
 
 var app = express();
@@ -22,6 +23,30 @@ app.use(bodyParser.urlencoded({extended : false}));
 
 // set static path
 app.use(express.static(path.join(__dirname, "public")));
+
+//global variables
+app.use(function (req, res,next) {
+  res.locals.errors = null;
+  next();
+})
+
+//express validator middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
 var person = [
   {
@@ -48,6 +73,28 @@ app.get('/',function(req,res){
   });
 });
 
+app.post("/users/add", function(req, res) {
+
+req.checkBody("name", "name is essential").notEmpty();
+req.checkBody("age", "age is very essential").notEmpty();
+var errors = req.validationErrors();
+
+if (errors) {
+  console.log("Error occured :(");
+  res.render("index",{
+    title: "customers",
+    users: person,
+    errors : errors
+  });
+} else {
+  var newUser = {
+    name : req.body.name,
+    age : req.body.age
+  };
+  console.log(newUser);
+}
+
+});
 
 app.listen(3000, function() {
 console.log("App started on port 3000!! :)");
